466758ba31c067b22bd35b00d7e44d4e007bae43
aedius <Aedius@users.noreply.github.com>
2026-04-17 22:36:09 +0200

    add callback_recall as a poc

--- planet/server/src/consumer/planet.rs
+++ planet/server/src/consumer/planet.rs
@@ -1,6 +1,8 @@
 use crate::PlanetRepository;
-use anyhow::{Context, Error};
-use chrono::Utc;
+use anyhow::Context;
+use horfimbor_callback_recall::SchedulerBuilder;
+use horfimbor_callback_recall::database::CallBack;
+use horfimbor_callback_recall::database::sqlite::open;
 use horfimbor_eventsource::helper::create_subscription;
 use horfimbor_eventsource::model_key::ModelKey;
 use horfimbor_eventsource::{Event, Stream};
@@ -9,7 +11,7 @@ use planet_shared::event::SharedPlanetEvent;
 use planet_state::PlanetCommand;
 use planet_state::PrivatePlanetCommand::FinnishConstruction;
 pub use std::time::Duration;
-use tokio::time::sleep;
+use uuid::Uuid;
 
 pub async fn handle_planet_start_building(
     event_store_db: Client,
@@ -20,6 +22,48 @@ pub async fn handle_planet_start_building(
         building: Default::default(),
         end: Default::default(),
     };
+
+    let db = open("test").await.context("cannot create sqlite db")?;
+
+    let mut builder = SchedulerBuilder::new(db, Duration::from_secs(2))
+        .await
+        .context("cannot create builder")?;
+
+    let planet_repository_register = planet_repository.clone();
+
+    builder.register(e.event_name(), move |payload| {
+        let planet_repository = planet_repository_register.clone();
+        async move {
+            let data = String::from_utf8(payload).map_err(|e| e.to_string())?;
+
+            let Some((model_key, key)) = data.split_once(":") else {
+                return Err(format!("cannot split: {data}"));
+            };
+
+            let model_key =
+                ModelKey::try_from(model_key).map_err(|e| format!("bad model_key: {}", e))?;
+
+            let key = Uuid::try_from(key).map_err(|e| format!("bad key: {}", e))?;
+
+            let _s = planet_repository
+                .clone()
+                .add_command(
+                    &model_key,
+                    PlanetCommand::Private(FinnishConstruction { key }),
+                    None,
+                )
+                .await
+                .map_err(|e| format!("cannot add command: {}", e))?;
+
+            Ok(())
+        }
+    });
+
+    let (emitter, listener) = builder.start();
+
+    tokio::spawn(async move {
+        listener.join().await;
+    });
 
     let stream = Stream::Event(e.event_name());
     let group_name = "mono_planet_start_building";
@@ -35,7 +79,6 @@ pub async fn handle_planet_start_building(
         .await
         .context("cannot subscribe")?;
 
-    let planet_repository = planet_repository.clone();
     loop {
         let rcv_event = sub.next().await.context("cannot get next event")?;
 
@@ -49,27 +92,14 @@ pub async fn handle_planet_start_building(
             ModelKey::try_from(event.stream_id()).context("cannot convert streamId to ModelKey")?;
 
         if let SharedPlanetEvent::UpdateConstruction { key, end, .. } = json {
-            let planet_repository = planet_repository.clone();
-
-            tokio::spawn(async move {
-                let now = Utc::now();
-
-                let to_wait = (end - now).num_seconds();
-                if to_wait > 0 {
-                    sleep(Duration::from_secs(to_wait as u64)).await;
-                }
-
-                let _s = planet_repository
-                    .add_command(
-                        &model_key,
-                        PlanetCommand::Private(FinnishConstruction { key }),
-                        None,
-                    )
-                    .await
-                    .context("cannot add command")?;
-
-                Ok::<(), Error>(())
-            });
+            emitter
+                .schedule(CallBack::new(
+                    e.event_name().to_string(),
+                    Vec::from(format!("{model_key}:{key}")),
+                    end,
+                ))
+                .await
+                .context("cannot schedule event")?;
         }
 
         sub.ack(&rcv_event).await.context("cannot ack")?;
