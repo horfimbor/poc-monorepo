a5940abcd14c750aedaddfd936edb9a1f3409c47
aedius <Aedius@users.noreply.github.com>
2026-06-05 22:00:54 +0200

    fix conflict

--- planet/shared/src/command.rs
+++ planet/shared/src/command.rs
@@ -14,6 +14,7 @@ use uuid::Uuid;
 #[cfg_attr(feature = "server", state(PLANET_STATE_NAME))]
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub enum SharedPlanetCommand {
+    ChangeName(String),
     Create {
         owner: String,
         user_id: String,
